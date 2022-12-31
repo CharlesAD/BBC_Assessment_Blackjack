c659a1764853cb8d4c2460cf2d32fa798d1bdc73
CharlesAD <c.acquahdavis@gamil.com>
2022-12-31 18:47:18 +0000

    Create deck function

---
+++ index.js
@@ -0,0 +1,14 @@
+let deck;
+let values;
+function createDeck(){
+    let values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+    let types = ["C", "D", "H", "S"];
+    deck = [];
+
+    for(i = 0; i < types.length; i++){
+        for(j = 0; j < values.length; j++){
+            deck.push(values[j] + "-" + types[i]);
+        }
+    }
+// console.log(deck);
+}
